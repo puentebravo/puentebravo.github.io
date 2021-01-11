5b82022197c8c85a289ef5bbe7a0fb5b2f53297a
Puentebravo <puentematos@gmail.com>
2021-01-10 20:39:30 -0500

    Added JS skeleton, .ready function, and object array

---
+++ assets/portfolio.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    console.log("linked!")
+    const portArray = [
+        {
+            name: "Squall", 
+            github: "https://github.com/puentebravo/Squall", 
+            deployed: "https://puentebravo.github.io/Squall/",
+            description: "Travel is complex, stressful, and unpredictable, and that's before taking into account what the weather will be once you reach your destination. Squall was designed to take some of the guesswork out of that equation, enabling a traveler on the go to easily see detailed current conditions in any city of their choosing, as well as a five day forecast detailing temperature, humidity, and real feel. Utilizing the openweathermap API and built using dynamically rendered HTML, JQUERY, and Bootstrap, Squall is built to have a low data footprint, and to be as responsive as possible to different devices.",
+            code: "HTML/CSS/Javascript/Bootstrap/JQUERY/JSON" },
+        {
+            name: "codeQuiz", 
+            github: "https://github.com/puentebravo/codeQuiz", 
+            deployed: "https://puentebravo.github.io/codeQuiz/index.html", 
+            description:"An interactive, dynamic, fun way for programming students to test their knowledge of basic coding principals in a timed, multiple choice format. With a clean, easy to navigate interface, Code Quiz gives clear, live feedback to students as they progress, allowing them to compare their score to that of their peers once the game is done. Code Quiz is made using dynamic javascript and styled using Bootstrap, making it responsive across multiple devices.", 
+            code: "HTML/CSS/Javascript/Bootstrap/JSON"},
+        {
+            name: "Farmwise Fresh Eats",
+            github: "https://github.com/ericfreyer/Farmwise_FreshEats",
+            deployed: "https://ericfreyer.github.io/Farmwise_FreshEats/",
+            description: "The truth is out there; maintaining a healthy, fresh, balanced diet is vital to your overall health and wellbeing, and the fresher the ingredients in your food, the better. Buying from a Farmer's market delivers the one-two punch of not only being good for yourself, but for your community and the environment along with it. But finding a farmer's market isn't always easy. Enter Farmwise Fresh Eats. Asking only for your zip code, our easy to use and intuitive interface shows you markets in your area at a glance. Simply click on a market to plot its location on a map, see details on goods sold, and how early (and late!) they're open. Farmwise Fresh Eats draws directly from the USDA's database of registered markets, ensuring that the data you see is as up to date and accurate as can be.",
+            code: "HTML/CSS/Javascript/Materialize/JQUERY/JSON"
+        }
+    ]
+
+    
+});
