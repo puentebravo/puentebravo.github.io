a9cb6f63030efca071ce978920e10f3e0fd21038
Puentebravo <puentematos@gmail.com>
2021-03-09 23:25:24 -0500

    Added detail to code used in portfolio projects

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
       img: "./assets/images/Burgatory.gif",
       deployed: "https://mysterious-fjord-09180.herokuapp.com/",
       description: "Burgatory is your one stop digital ordering interface. Hand crafted for Burgatory, a proudly rustic dining establishment, the app allows a customer to create an order, mark it devoured, then trash it. Responses and information are logged to a SQL database, making it easier than ever to keep track of what is being eaten. Powered by Javascript, node, handlebars, and SQL, Burgatory is clean, powerful, delicious fun.",
-      code: "HTML/CSS/Handlebars/SQL/JavaScript/JSON/Bootstrap/Node.js"
+      code: "HTML/CSS/Handlebars/SQL/JavaScript/JSON/Bootstrap/Node.js/Express"
     },
     {
       name: "From Dad To Bad",
@@ -23,7 +23,7 @@ $(document).ready(function () {
       img: "./assets/images/Dadabase.gif",
       deployed: "https://aqueous-meadow-09339.herokuapp.com/",
       description: "From Dad to Bad is your one stop shop for all things dad jokes, puns, and movie one liners. Whether it elicits laughter, tears, groaning, or a mixture of all three, From Dad to Bad has it all!",
-      code: "HTML/CSS/Handlebars/SQL/Sequelize/JavaScript/JSON/Bootstrap/Node.js"
+      code: "HTML/CSS/Handlebars/SQL/Sequelize/JavaScript/JSON/Bootstrap/Node.js/Express"
     },
   ];
 
