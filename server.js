14f158872de5da0cd99790b12e844998292d8537
Puentebravo <puentematos@gmail.com>
2021-03-15 17:56:02 -0400

    Added middleware to serve static files

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const path = require('path')
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 
+app.use(express.static("assets"))
 
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
 
+app.get("/portfolio", (req, res) => {
+  res.sendFile(path.join(__dirname, "./portfolio.html"));
+});
 
-app.listen(PORT, function() {
-    console.log(`server listening on port ---> ${PORT}`)
-})
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "./contact.html"));
+});
+
+app.listen(PORT, function () {
+  console.log(`server listening on port ---> ${PORT}`);
+});
