ff81bc80a2a8836ead0e57526504309336c7f5e4
Puentebravo <puentematos@gmail.com>
2021-04-03 20:28:29 -0400

    Transffered legacy HTML to contact card component, translated to JSX

---
+++ src/components/contactCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function ContactCard() {
+  return (
+    <div className="container">
+      <div className="row d-flex justify-content-center">
+        <div className="card col-lg-8">
+          <h1 className="display-4">Get in touch!</h1>
+          <hr className="my-4" />
+          <p className="lead">Follow me here:</p>
+
+          <a className="btn btn-dark" href="https://github.com/puentebravo">
+            <i className="fab fa-github-square fa-lg cIcon"></i> Github
+          </a>
+          <a
+            className="btn btn-dark"
+            href="https://www.linkedin.com/in/puentematos/"
+          >
+            <i className="fab fa-linkedin fa-lg cIcon"></i>LinkedIn
+          </a>
+          <br />
+          <br />
+          <p className="lead">
+            Looking for a quick, to the point summary? Grab my resume below:
+          </p>
+          <a href="assets/documents/charliepuentecv.pdf">CV Download (PDF)</a>
+
+          <hr class="my-4" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactCard;
