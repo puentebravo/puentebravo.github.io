a4e18ef0218665c14221f31ddf82591d63252257
Puentebravo <puentematos@gmail.com>
2021-04-05 15:49:42 -0400

    Fixed footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+function Footer() {
+    return (
+      <footer className="footer bg-dark">
+        <div className="container">
+          <span className="text-left"> (©) 2020 Charlie Puente |</span>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
