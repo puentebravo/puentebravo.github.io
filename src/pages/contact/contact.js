0bdae5cf9f14cdbedc5be531b6c2a71551f16256
Puentebravo <puentematos@gmail.com>
2021-04-05 19:06:45 -0400

    Added styling, JSX for contact page

---
+++ src/pages/contact/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/header";
+import ContactCard from "../../components/contactCard";
+import Footer from "../../components/footer";
+import "./contact.css";
+
+function Contact() {
+    return (
+        <Header/>,
+        <ContactCard/>,
+        <Footer/>
+    )
+}
+
+export default Contact;
