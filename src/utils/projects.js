7a80cb40d8333854f23d09454a91aa4a51503922
Puentebravo <puentematos@gmail.com>
2021-04-05 16:21:08 -0400

    fixed relative paths to image files

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,7 +2,7 @@ const projectList = [
   {
     name: "Squall",
     github: "https://github.com/puentebravo/Squall",
-    img: "assets/images/Squall.gif",
+    img: "../assets/images/Squall.gif",
     deployed: "https://puentebravo.github.io/Squall/",
     description:
       "Travel is complex, stressful, and unpredictable, and that's before taking into account what the weather will be once you reach your destination. Squall was designed to take some of the guesswork out of that equation, enabling a traveler on the go to easily see detailed current conditions in any city of their choosing, as well as a five day forecast detailing temperature, humidity, and real feel. Utilizing the openweathermap API and built using dynamically rendered HTML, JQUERY, and Bootstrap, Squall is built to have a low data footprint, and to be as responsive as possible to different devices.",
@@ -11,7 +11,7 @@ const projectList = [
   {
     name: "Burgatory",
     github: "https://github.com/puentebravo/Burgatory",
-    img: "assets/images/Burgatory.gif",
+    img: "../assets/images/Burgatory.gif",
     deployed: "https://mysterious-fjord-09180.herokuapp.com/",
     description:
       "Burgatory is your one stop digital ordering interface. Hand crafted for Burgatory, a proudly rustic dining establishment, the app allows a customer to create an order, mark it devoured, then trash it. Responses and information are logged to a SQL database, making it easier than ever to keep track of what is being eaten. Powered by Javascript, node, handlebars, and SQL, Burgatory is clean, powerful, delicious fun.",
@@ -20,7 +20,7 @@ const projectList = [
   {
     name: "From Dad To Bad",
     github: "https://github.com/puentebravo/project2",
-    img: "assets/images/Dadabase.gif",
+    img: "../assets/images/Dadabase.gif",
     deployed: "https://aqueous-meadow-09339.herokuapp.com/",
     description:
       "From Dad to Bad is your one stop shop for all things dad jokes, puns, and movie one liners. Whether it elicits laughter, tears, groaning, or a mixture of all three, From Dad to Bad has it all!",
