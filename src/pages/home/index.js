bcad1d12675076658873887447469b7df38cbe22
Puentebravo <puentematos@gmail.com>
2021-04-05 19:07:07 -0400

    Added JSX and styling, linked stylesheet to homepage.

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/header";
+import LeadCard from "../../components/leadCard";
+import Footer from "../../components/footer";
+import "./index.css";
+
+function Index() {
+    return (
+        <Header/>,
+        <LeadCard/>,
+        <Footer/>
+    )
+}
+
+export default Index;
