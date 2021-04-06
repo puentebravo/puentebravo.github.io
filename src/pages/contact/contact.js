448be3c4f396f3a2caadc8af8567b72aeb4009da
Puentebravo <puentematos@gmail.com>
2021-04-06 14:09:11 -0400

    Added spacer and CSS for placement, as well as button spacing

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header";
 import ContactCard from "../../components/contactCard";
+import Spacer from "../../components/spacer";
 import Footer from "../../components/footer";
 import "./contact.css";
 
@@ -9,6 +10,7 @@ function Contact() {
         <div>
         <Header/>,
         <ContactCard/>,
+        <Spacer/>
         <Footer/>
         </div>
     )
