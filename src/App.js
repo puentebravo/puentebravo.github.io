dab483d60f790c1d1f76741cc729183f7373ad56
Puentebravo <puentematos@gmail.com>
2021-04-05 20:57:54 -0400

    Fixed react router, added correct import statement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/home/index";
+import Index from "./pages/home/index";
 import Portfolio from "./pages/portfolio/portfolio";
 import Contact from "./pages/contact/contact";
 
@@ -9,14 +9,12 @@ import "./App.css";
 function App() {
   return (
     <Router basename={`${process.env.PUBLIC_URL}/`}>
-      <div>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/" component={Index} />
+          <Route exact path="/home" component={Index} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
         </Switch>
-      </div>
     </Router>
   );
 }
