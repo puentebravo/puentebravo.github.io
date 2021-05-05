b8d87623a369b78e4ba1553c8f00f23756f98a57
Puentebravo <puentematos@gmail.com>
2021-05-05 12:50:09 -0400

    fixed spacing error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Index from "./pages/home/index";
 import Portfolio from "./pages/portfolio/portfolio";
 import Contact from "./pages/contact/contact";
-
 import "./App.css";
 
 function App() {
