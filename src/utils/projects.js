56cff74dafc9a9fa94c4c2c228dd3847c4e6634c
Charlie Puente <puentematos@gmail.com>
2022-07-22 17:45:25 -0400

    Removed superannuated projects from portfolio

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -11,38 +11,21 @@ const projectList = [
   {
     name: "KeyGen 2.0",
     github: "https://github.com/puentebravo/KeyGen-2.0",
-    img: "./images/keyGen.png",
+    img: "./images/keygen.png",
     deployed: "https://gentle-sea-23195.herokuapp.com/",
     description:
       "Security is paramount on the modern web, and using a different password for each site and account you use is almost universally considered a best practice. Creating a strong password for each, however, can be time consuming. KeyGen allows for the fast, easy creation of passwords of almost any desireable length in less than a second.",
     code: "HTML/CSS/Javascript/Bootstrap/React.js/JSON",
   },
   {
-    name: "Burgatory",
-    github: "https://github.com/puentebravo/Burgatory",
-    img: "./images/Burgatory.gif",
-    deployed: "https://mysterious-fjord-09180.herokuapp.com/",
-    description:
-      "Burgatory is your one stop digital ordering interface. Hand crafted for Burgatory, a proudly rustic dining establishment, the app allows a customer to create an order, mark it devoured, then trash it. Responses and information are logged to a SQL database, making it easier than ever to keep track of what is being eaten. Powered by Javascript, node, handlebars, and SQL, Burgatory is clean, powerful, delicious fun.",
-    code: "HTML/CSS/Handlebars/SQL/JavaScript/JSON/Bootstrap/Node.js/Express",
-  },
-  {
     name: "TCell",
     github: "https://github.com/puentebravo/TCell",
     img: "./images/Tcell.png",
-    deployed: "https://drive.google.com/file/d/1QqLvRhAWo2A6tQVcAiTBpV7cEJaXLM4B/view",
+    deployed:
+      "https://drive.google.com/file/d/1QqLvRhAWo2A6tQVcAiTBpV7cEJaXLM4B/view",
     description:
       "Powered by Node.js and Inquirer, TCELL is a Command Line Interface that allows teams to make a clean, professional organizational chart in minutes.",
     code: "HTML/CSS/Javascript/Bootstrap/Node.js/JSON/Inquirer/Jest",
-  },
-  {
-    name: "ReadmeGen",
-    github: "https://github.com/puentebravo/ReadmeGen",
-    img: "./images/Readmegen.png",
-    deployed: "https://drive.google.com/file/d/1lVZJA35eUtbiLd5bZx4KlapMuClAhudb/view",
-    description:
-      "ReadmeGen was created to help simplify one of the most vital aspects of development; README documentation. Using Node.js and Inquirer, ReadmeGen uses a short battery of questions to fill out and format concisely written documentation.",
-    code: "Javascript/Node.js/Inquirer",
   },
   {
     name: "From Dad To Bad",
@@ -51,8 +34,7 @@ const projectList = [
     deployed: "https://aqueous-meadow-09339.herokuapp.com/",
     description:
       "From Dad to Bad is your one stop shop for all things dad jokes, puns, and movie one liners. Whether it elicits laughter, tears, groaning, or a mixture of all three, From Dad to Bad has it all!",
-    code:
-      "HTML/CSS/Handlebars/SQL/Sequelize/JavaScript/JSON/Bootstrap/Node.js/Express",
+    code: "HTML/CSS/Handlebars/SQL/Sequelize/JavaScript/JSON/Bootstrap/Node.js/Express",
   },
 ];
 
