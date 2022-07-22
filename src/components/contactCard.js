0addae0178c3f54ce24de9240a933f5bf9d2a328
Charlie Puente <puentematos@gmail.com>
2022-07-22 17:41:36 -0400

    Changed contact card styling, bg color, and button color

--- src/components/contactCard.js
+++ src/components/contactCard.js
@@ -4,20 +4,20 @@ function ContactCard(props) {
   return (
     <div className="container">
       <div className="row d-flex justify-content-center">
-        <div className="card col-lg-8" id={props.id}>
+        <div className="card col-lg-8 bg-dark text-white border border-light" id={props.id}>
           <div className="card-header">
           <h1 className="display-4 text-center">Get in touch!</h1>
           </div>
           <p className="lead text-center">Follow me here:</p>
 
           <a
-            className="btn btn-dark cardBtn"
+            className="btn btn-warning cardBtn"
             href="https://github.com/puentebravo"
           >
             <i className="fab fa-github-square fa-lg cIcon"></i> Github
           </a>
           <a
-            className="btn btn-dark cardBtn"
+            className="btn btn-warning cardBtn"
             href="https://www.linkedin.com/in/puentematos/"
           >
             <i className="fab fa-linkedin fa-lg cIcon"></i>LinkedIn
@@ -25,7 +25,7 @@ function ContactCard(props) {
           <br />
           <p className="lead text-center">...Or contact me directly at:</p>
           <a
-            className="btn btn-dark cardBtn"
+            className="btn btn-warning cardBtn"
             href="mailto:puentematos@gmail.com"
           ><i className="fas fa-envelope-square fa-lg cIcon"></i>puentematos@gmail.com</a>
           <br />
@@ -34,7 +34,7 @@ function ContactCard(props) {
             <p className="lead">
               Looking for a quick, to the point summary? Grab my resume below:
             </p>
-            <a className="btn btn-dark cardBtn" href="./documents/charliepuentecv.pdf">CV Download (PDF)</a>
+            <a className="btn btn-warning cardBtn" href="./documents/charliepuentecv.pdf">CV Download (PDF)</a>
           </div>
         </div>
       </div>
