93b59e66c488b2ff54c2061146108ce5e0c1fc63
Puentebravo <puentematos@gmail.com>
2022-11-03 18:47:56 -0400

    Added glass styling to contact card, changed color scheme to match overall site

--- src/components/contactCard.js
+++ src/components/contactCard.js
@@ -12,28 +12,28 @@ function ContactCard(props) {
   return (
     <div className="container">
       <div className="row d-flex justify-content-center">
-        <div className="card col-lg-8 bg-dark text-white" id={props.id}>
+        <div className="card col-lg-8 dark" id={props.id}>
           <div className="card-header">
-          <h1 className="display-4 text-center">Get in touch!</h1>
+          <h1 className="display-4 text-center text-flight">Get in touch!</h1>
           </div>
           <p className="lead text-center">Follow me here:</p>
 
           <a
-            className="btn btn-light cardBtn"
+            className="btn btn-ultra btn-ultra-glow cardBtn"
             href="https://github.com/puentebravo"
           >
             <i className="fab fa-github-square fa-lg cIcon"></i> Github
           </a>
           <a
-            className="btn btn-light cardBtn"
+            className="btn btn-ultra btn-ultra-glow cardBtn"
             href="https://www.linkedin.com/in/puentematos/"
           >
             <i className="fab fa-linkedin fa-lg cIcon"></i>LinkedIn
           </a>
           <br />
-          <p className="lead text-center">...Or contact me directly at:</p>
+          <p className="lead text-center text-bright">...Or contact me directly at:</p>
           <a
-            className="btn btn-light cardBtn"
+            className="btn btn-ultra btn-ultra-glow cardBtn"
             href="mailto:puentematos@gmail.com"
           ><i className="fas fa-envelope-square fa-lg cIcon"></i>puentematos@gmail.com</a>
           <br />
@@ -42,17 +42,17 @@ function ContactCard(props) {
             <p className="lead">
               Looking for a quick, to the point summary? Grab my resume below:
             </p>
-            <button className="btn btn-light cardBtn" onClick={handleShow}>CV Download (PDF)</button>
+            <button className="btn btn-ultra btn-ultra-glow cardBtn" onClick={handleShow}>CV Download (PDF)</button>
             <Modal show={showModal} onHide={processClose} {...props} size="lg" centered>
               <Modal.Header className="bg-dark text-white">
                 <Modal.Title><h3>Charlie's Resume</h3></Modal.Title>
               </Modal.Header>
-              <Modal.Body className="bg-light">
+              <Modal.Body className="bg-dark">
               <embed src={resume} width="100%" height="800px" />
               </Modal.Body>
               <Modal.Footer className="bg-dark">
-                <a className="btn btn-light cardBtn" href={resume}>Download</a>
-                <button className="btn btn-light cardBtn" onClick={processClose}>Close</button>
+                <a className="btn btn-ultra btn-ultra-glow cardBtn" href={resume}>Download</a>
+                <button className="btn btn-ultra btn-ultra-glow cardBtn" onClick={processClose}>Close</button>
               </Modal.Footer>
             </Modal>
           </div>
