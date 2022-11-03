7996e52638cd4e5d0450945e287789f74bf86a65
Puentebravo <puentematos@gmail.com>
2022-11-03 18:49:13 -0400

    Changed styling to match page theme, added glass styling to card component

--- src/components/leadCard.js
+++ src/components/leadCard.js
@@ -5,28 +5,28 @@ function LeadCard() {
   return (
     <div className="container">
       <div className="row">
-        <div className="card bg-dark text-white col-lg">
+        <div className="card dark col-lg">
           <img
             className="img-fluid mx-auto img-thumbnail mt-2"
             src={Galapagos}
             alt="Photographing Frigate Birds in the Galapagos"
             id="leadImg"
           />
-          <p className="lead text-center text-muted">
+          <p className="lead text-center text-silver">
             Charlie Puente | Photo credit: Maya Caballero
           </p>
           <hr />
-          <h1 className="display-4 text-center">
+          <h1 className="display-4 text-center text-flight">
             Charlie Puente: Writing Stories and Code
           </h1>
-          <p className="lead text-center">
+          <p className="lead text-center text-bright">
             Born in New York, but raised in China and Japan, I grew up around
             people as diverse as the cultures and places I experienced. With a
             family background in science, medicine, engineering, and old
             fashioned storytelling, I love learning and implementing new
             technology as much as I enjoy writing about it.
           </p>
-          <p className="lead text-center">
+          <p className="lead text-center text-bright">
             After graduating from Columbia University's School of Engineering
             coding bootcamp, I work as a Full Stack Web Developer specializing
             in the MERN stack. Feel free to browse my work, or get in touch with me on LinkedIn!
