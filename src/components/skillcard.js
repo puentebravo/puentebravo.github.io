d3655b6d6ca67a7036de50ad976ac247820f959c
Puentebravo <puentematos@gmail.com>
2022-11-03 18:52:12 -0400

    Added glass styling, page themes to skill card

--- src/components/skillcard.js
+++ src/components/skillcard.js
@@ -2,37 +2,37 @@ import React from "react";
 
 function SkillCard() {
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="card bg-dark text-white col-lg">
+    <section className="container">
+      <div className="row justify-content-between">
+        <div className="card dark col-lg">
           <div className="card-header">
-            <p className="lead text-center text-white">Skills at a glance</p>
+            <p className="lead text-center text-flight">Skills at a glance</p>
           </div>
           <div className="card-body mx-auto">
             <div className="row d-flex">
               <div className="col-2">
-                <i className="fab fa-html5 fa-3x sIcon" />
+                <i className="fab fa-html5 fa-3x sIcon text-flight" />
               </div>
               <div className="col-2">
-                <i className="fab fa-css3-alt fa-3x sIcon" />
+                <i className="fab fa-css3-alt fa-3x sIcon text-flight" />
               </div>
               <div className="col-2">
-                <i className="fab fa-react fa-3x sIcon" />
+                <i className="fab fa-react fa-3x sIcon text-flight" />
               </div>
               <div className="col-2">
-                <i className="fab fa-node fa-3x sIcon" />
+                <i className="fab fa-node fa-3x sIcon text-flight" />
               </div>
               <div className="col-2">
-                <i className="fab fa-python fa-3x sIcon" />
+                <i className="fab fa-python fa-3x sIcon text-flight" />
               </div>
               <div className="col-2">
-                <i className="fab fa-js-square fa-3x sIcon" />
+                <i className="fab fa-js-square fa-3x sIcon text-flight" />
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
