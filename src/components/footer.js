220699176d93fb63bb8d4a8bd0ec9ea4ec677b89
Puentebravo <puentematos@gmail.com>
2022-11-03 18:48:31 -0400

    changed styling to match page, moved footer text to start

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 function Footer() {
-    return (
-      <footer className="footer bg-dark">
-        <div className="container">
-          <span className="text-muted text-center"> (©) 2020 Charlie Puente |</span>
-        </div>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <footer className="footer dark">
+      <p className="text-silver"> (©) 2020 Charlie Puente |</p>
+    </footer>
+  );
+}
+
+export default Footer;
