fbb0e1e122afbc1a71f5799c78a6fbec325dc5aa
Puentebravo <puentematos@gmail.com>
2022-11-03 18:50:33 -0400

    add glass styling, changed CSS to match page theme

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -6,23 +6,25 @@ function ProjectCard({ projects }) {
     <div className="container" id={projects.section} key={projects.id}>
       
         <div className="row justify-content-center" >
-          <div className="card bg-dark text-white col-lg p-4">
+          <div className="card dark col-lg p-4">
             <img className="card-img" alt="site demo" src={projects.img} />
-            <h5 className="card-title text-center">{projects.name}</h5>
-            <p className="card-text">{projects.description}</p>
-            <p className="card-text text-muted text-center">
+            <h5 className="card-title text-center text-flight mt-3">{projects.name}</h5>
+            <p className="card-text text-bright">{projects.description}</p>
+            <p className="card-text text-silver text-center">
               {projects.code}
             </p>
             <hr />
-            <div className="row">
+            <div className="row d-flex justify-content-center">
               <a
-                className="btn btn-light col-lg-12 cardBtn"
+                className="btn btn-ultra btn-ultra-glow col-lg-6 cardBtn"
                 href={projects.github}
               >
                 Deployed<i className="fas fa-globe-asia cIcon"></i>  
               </a>
+            </div>
+            <div className="row d-flex justify-content-center">
               <a
-                className="btn btn-light col-lg-12 cardBtn"
+                className="btn btn-ultra btn-ultra-glow col-lg-6 cardBtn"
                 href={projects.deployed}
               >
                 Github<i className="fab fa-github cIcon"></i>
