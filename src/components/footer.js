74655903db4fd50b6452ed339656eefcb6022866
Puentebravo <puentematos@gmail.com>
2022-11-03 19:24:21 -0400

    Updated copyright

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer dark">
-      <p className="text-silver"> (©) 2020 Charlie Puente |</p>
+      <p className="text-silver"> (©) 2020-2022 Charlie Puente |</p>
     </footer>
   );
 }
