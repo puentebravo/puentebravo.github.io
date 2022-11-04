ba94f314a6f3dc242f12ef3e837dfa2fd8755e47
Puentebravo <puentematos@gmail.com>
2022-11-04 14:39:44 -0400

    Fixed skill card icon rendering on mobile

--- src/components/skillcard.js
+++ src/components/skillcard.js
@@ -6,26 +6,26 @@ function SkillCard() {
       <div className="row justify-content-between">
         <div className="card dark col-lg">
           <div className="card-header">
-            <p className="lead text-center text-flight">Skills at a glance</p>
+            <h4 className="display-5 text-center text-flight">Skills at a glance</h4>
           </div>
           <div className="card-body mx-auto">
-            <div className="row d-flex">
-              <div className="col-2">
+            <div className="row d-flex justify-content-center">
+              <div className="col-2 p-0 m-2">
                 <i className="fab fa-html5 fa-3x sIcon text-flight" />
               </div>
-              <div className="col-2">
+              <div className="col-2 p-0 m-2">
                 <i className="fab fa-css3-alt fa-3x sIcon text-flight" />
               </div>
-              <div className="col-2">
+              <div className="col-2 p-0 m-2">
                 <i className="fab fa-react fa-3x sIcon text-flight" />
               </div>
-              <div className="col-2">
+              <div className="col-2 p-0 m-2">
                 <i className="fab fa-node fa-3x sIcon text-flight" />
               </div>
-              <div className="col-2">
+              <div className="col-2 p-0 m-2">
                 <i className="fab fa-python fa-3x sIcon text-flight" />
               </div>
-              <div className="col-2">
+              <div className="col-2 p-0 m-2">
                 <i className="fab fa-js-square fa-3x sIcon text-flight" />
               </div>
             </div>
